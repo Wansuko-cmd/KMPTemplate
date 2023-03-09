99e4f2d3fd3954262589e6f9473ccd8c40c671ac
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-03-09 22:49:21 +0900

    buildSrcを追加

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    `kotlin-dsl`
+}
+
+dependencies {
+    implementation(libs.gradle.kotlin)
+    implementation(libs.gradle.android)
+    implementation(libs.gradle.hilt)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "com.template.application"
+            implementationClass = "plugins.AndroidApplicationPlugin"
+        }
+        register("androidLibrary") {
+            id = "com.template.library"
+            implementationClass = "plugins.AndroidLibraryPlugin"
+        }
+        register("daggerHilt") {
+            id = "com.template.dagger-hilt"
+            implementationClass = "plugins.DaggerHiltPlugin"
+        }
+    }
+}
---
+++ buildSrc/src/main/java/Plugins.kt
@@ -0,0 +1,5 @@
+object Plugins {
+    const val androidApplication = "com.template.application"
+    const val androidLibrary = "com.template.library"
+    const val daggerHilt = "com.template.dagger-hilt"
+}
