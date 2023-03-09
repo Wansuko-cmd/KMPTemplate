7362360d9b540a4cf59436d4d42c4adaf3a5d738
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-03-09 19:24:25 +0900

    [kts] setting.gradleをkts化

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,21 @@
+@file:Suppress("UnstableApiUsage")
+enableFeaturePreview("VERSION_CATALOGS")
+
 pluginManagement {
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
-        gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        gradlePluginPortal()
         google()
         mavenCentral()
     }
 }
+
 rootProject.name = "Template"
-include ':app'
+include(":app")
