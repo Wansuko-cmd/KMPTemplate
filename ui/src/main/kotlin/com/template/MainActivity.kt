cecb12cb0fd2c3c891b88633e0e837435fb29114
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2023-03-13 18:18:19 +0900

    [fix] UIまわりのライブラリのコードを修正

--- ui/src/main/kotlin/com/template/MainActivity.kt
+++ ui/src/main/kotlin/com/template/MainActivity.kt
@@ -1,5 +1,20 @@
 package com.template
 
+import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import com.template.theme.TemplateTheme
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity()
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TemplateTheme {
+                Text(text = "Hello World")
+            }
+        }
+    }
+}
