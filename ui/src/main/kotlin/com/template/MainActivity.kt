9ac0619ba255554c31b5ae657f4beeb7177ba54d
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2024-04-01 18:21:43 +0900

    [ktlint] Formatを実行

--- ui/src/main/kotlin/com/template/MainActivity.kt
+++ ui/src/main/kotlin/com/template/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import com.template.theme.TemplateTheme
-import dagger.hilt.android.AndroidEntryPoint
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
