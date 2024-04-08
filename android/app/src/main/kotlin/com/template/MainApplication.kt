761c219dbfa82d017dff1752245609e6571b4510
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2024-04-08 12:02:49 +0900

    [move] Acvitivyをappに移動

--- android/app/src/main/kotlin/com/template/MainApplication.kt
+++
@@ -1,5 +0,0 @@
-package com.template
-
-import android.app.Application
-
-class MainApplication : Application()
---
+++ android/ui/src/main/kotlin/com/template/MainView.kt
@@ -0,0 +1,12 @@
+package com.template
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.template.theme.TemplateTheme
+
+@Composable
+fun MainView() {
+    TemplateTheme {
+        Text(text = "Hello World")
+    }
+}
