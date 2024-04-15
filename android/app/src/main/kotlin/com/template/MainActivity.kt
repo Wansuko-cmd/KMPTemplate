31a8eefda6bb72787f278a269a20b231950edb69
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2024-04-15 13:14:57 +0900

    test

--- android/app/src/main/kotlin/com/template/MainActivity.kt
+++ android/app/src/main/kotlin/com/template/MainActivity.kt
@@ -7,8 +7,12 @@ import androidx.activity.compose.setContent
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        list.forEach(::println)
         setContent {
             MainView()
+
         }
     }
 }
+
+private val list = (1..100).filter { it % 3 == 0 }
